acd690b5cbf87540c1d588201e80a63a83124567
DESKTOP-VPF8PJ3\SKPS <skps2000@gmail.com>
2018-01-16 16:13:31 +0900

    INIT

---
+++ src/main/java/com/data/center/DataCenterApplication.java
@@ -0,0 +1,12 @@
+package com.data.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCenterApplication.class, args);
+	}
+}
